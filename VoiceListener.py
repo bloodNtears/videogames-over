fe428f370dd0d94a74b04c4c72a069cf59a3bab3
bunnyballzzz <60396819+bunnyballzzz@users.noreply.github.com>
2021-03-09 22:46:34 +0300

    Add files via upload
    need to feel the second table in the databases

--- VoiceListener.py
+++ VoiceListener.py
@@ -1,12 +1,26 @@
 from discord.ext import commands
 import discord
-from threading import Thread
 import datetime
 import sqlite3
 
-def fill_data(member, ds_id, cur, conn):
-    cur.execute('''INSERT OR IGNORE INTO UsersID (user, ds_id) Values ( ?, ? )''', (member, ds_id,))
 
+def fill_id_table(member):
+    conn = sqlite3.connect('projectdis.db')
+    cur = conn.cursor()
+    cur.execute('''SELECT * FROM UsersID WHERE ds_id = ?''', (member.id,))
+    exists = cur.fetchone()
+    if not exists:
+        cur.execute('''INSERT OR IGNORE INTO UsersID (user, ds_id) Values ( ?, ? )''', (member, member.id,))
+    print(exists)
+    conn.commit()
+    conn.close()
+
+
+def fill_time_db(member1, join_time1, member2, join_time2):
+    conn = sqlite3.connect('projectdis.db')
+    cur = conn.cursor()
+    user1_id = cur.execute('SELECT user_id from UsersID WHERE ds_id = ?', (member1.id,)).fetchone()
+    user2_id = cur.execute('SELECT user_id from UsersID WHERE ds_id = ?', (member2.id,)).fetchone()
 
 
 class VoiceListener(commands.Cog):
@@ -18,8 +32,8 @@ class VoiceListener(commands.Cog):
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
 
-        #active_users = dict()  # all users who are in voice chat rn; structure: {user1: join_time, user2: join_time ...}
-        #pairs = dict()  # dict of pairs related to a specific voice channel;
+        # active_users = dict()  # all users who are in voice chat rn; structure: {user1: join_time, user2: join_time ...}
+        # pairs = dict()  # dict of pairs related to a specific voice channel;
         # structure: {voice_channel1_id: [user1, user2, user3],
         # voice_channel2_id: [user4, user5, user6]
         # }
@@ -35,18 +49,11 @@ class VoiceListener(commands.Cog):
             if before.channel is None:
                 print(member.name, 'has connected to', after.channel.name, '\n')
 
-                conn = sqlite3.connect('projectdis.db')
-                cur = conn.cursor()
-                cur.execute('''SELECT * FROM UsersID WHERE ds_id = ?''', (member.id,))
-                exists = cur.fetchone()
-                print(exists)
-                if not exists:
-                    fill_data(member.name, member.id, cur, conn)
-                conn.commit()
-                conn.close()
+                fill_id_table(member)
 
                 self.active_users[member.id] = datetime.datetime.now().time()
                 print(self.active_users)
+
                 self.pairs.setdefault(after.channel.id, []).append(member.id)
                 print(self.pairs)
 
@@ -55,20 +62,29 @@ class VoiceListener(commands.Cog):
                 print(member.name, 'has connected to', after.channel.name, '\n')
                 # заполняем бд данными из active_users
                 self.pairs[before.channel.id].remove(member.id)
+                if not self.pairs[before.channel.id]:
+                    del self.pairs[before.channel.id]
                 self.active_users[member.id] = datetime.datetime.now().time()
                 print(self.active_users)
                 self.pairs.setdefault(after.channel.id, []).append(member.id)
                 print(self.pairs)
             else:
                 print(member.name, 'has disconnected from', before.channel.name, '\n')
-                del self.active_users[member.id]
-                del self.pairs[before.channel.id]
+                try:
+                    del self.active_users[member.id]
+                    del self.pairs[before.channel.id]
+                except Exception as e:
+                    print(e)
+                    pass
+                print(self.active_users)
+                print(self.pairs)
 
-
+        '''
         if before.self_mute is False and after.self_mute is True:
             print(member.name, 'has muted in', before.channel.name, '\n')
         if before.self_mute is True and after.self_mute is False:
             print(member.name, 'has unmuted in', before.channel.name, '\n')
+        '''
 
 
 def setup(bot):
