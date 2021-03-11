69672ca7613fda2002b53978131af9bb9040e035
Alexander <68970161+bloodNtears@users.noreply.github.com>
2021-03-11 16:29:02 +0300

    Add files via upload
    new funcs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 from VoiceListener import *
 
-TOKEN = ''
+TOKEN = 'ODA0Njk3NTYxNzc2MjU5MDgy.YBQHAQ.-aII_Q52f7weyP6O0-5bnBYmkxE'
 
 
 class MyClient(commands.Bot):
@@ -22,10 +22,39 @@ class MyClient(commands.Bot):
 
     # dm
     async def on_member_join(self, member):
+        def add_id_data(member):
+            print('adding user_id in UsersID')
+            conn = sqlite3.connect('projectdis.db')
+            cur = conn.cursor()
+            cur.execute('''SELECT * FROM UsersID WHERE ds_id = ?''', (member.id,))
+            exists = cur.fetchone()
+            if not exists:
+                cur.execute('''INSERT OR IGNORE INTO UsersID (user, ds_id) Values ( ?, ? )''',
+                            (member.name, member.id,))
+            user_id = cur.execute(f'SELECT * FROM UsersID WHERE ds_id = {member.id}').fetchone()[0]
+            print(exists)
+            conn.commit()
+            conn.close()
+            return user_id
+
+        def add_time_data(user_id):
+            print('adding in time')
+            conn = sqlite3.connect('projectdis.db')
+            cur = conn.cursor()
+            lst = cur.execute('select * from Time')
+            names = [description[0] for description in cur.description]
+            if str(user_id) not in names:
+                req = 'ALTER TABLE Time ADD sss' + str(user_id) + ' INTEGER;'
+                cur.execute(req)
+            # print(cur.description)
+
+        user_id = add_id_data(member)
+        print(user_id)
+        add_time_data(user_id)
         await member.send(f'Hi {member.name}, welcome to the club')
 
 
 bot = MyClient(command_prefix='!', intents=discord.Intents.all())
-setup(bot)
+#setup(bot)
 
 bot.run(TOKEN)
